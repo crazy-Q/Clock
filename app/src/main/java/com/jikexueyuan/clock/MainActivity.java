31ad208677a1e33d8ccb223d4cc5c34e854322da
crazy-Q <qmx754@163.com>
2016-08-11 09:52:02 +0800

    应用主框架 TabHost

--- app/src/main/java/com/jikexueyuan/clock/MainActivity.java
+++ app/src/main/java/com/jikexueyuan/clock/MainActivity.java
@@ -2,12 +2,25 @@ package com.jikexueyuan.clock;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabHost tabHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabHost = (TabHost) findViewById(R.id.tabHost);
+        tabHost.setup();//初始化tabhost
+
+        tabHost.addTab(tabHost.newTabSpec("tabTime").setIndicator("时钟").setContent(R.id.id_tab_time));
+        tabHost.addTab(tabHost.newTabSpec("tabAlarm").setIndicator("闹钟").setContent(R.id.id_tab_alarm));
+        tabHost.addTab(tabHost.newTabSpec("tabTimer").setIndicator("计时器").setContent(R.id.id_tab_timer));
+        tabHost.addTab(tabHost.newTabSpec("tabStopWatch").setIndicator("秒表").setContent(R.id.id_tab_stop_watch));
+
+
     }
 }
